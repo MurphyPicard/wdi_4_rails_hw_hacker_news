a4c9654b083da1c80f403bf669caa4ebaa72130d
murphypicard <ayaghsizian@gmail.com>
2016-12-23 15:31:02 -0500

    validates that there is a title and body

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'articles/show'
+
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/create'
+
+  get 'articles/update'
+
+  get 'articles/edit'
+
+  get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'articles#index'
